4054e1ed9c5d16c296e396d5f6fc77752e97fead
Alexander Sopov <alexander@bobbyhouston.se>
2017-04-29 13:54:11 +0200

    plenty of changes, especially to form. Almost done with backend

--- dev/static/js/nav.js
+++ dev/static/js/nav.js
@@ -1,12 +1,20 @@
 module.exports = (function(){
-	var menu = $("header div.menu");;
+	var menu = $("header div.menu");
 
 	function showMenu(logo){
 		if($(window).width() < 750)
 		menu.toggleClass("visible");
 	}
 
+	function goTo(selector){
+	    $('html, body').animate({
+    	    scrollTop: $(selector).offset().top
+    	}, 500);
+		//window.scrollTo(0, $(selector).offset().top);
+	}
+
 	return {
+		goTo:goTo,
 		showMenu:showMenu
 	}
 })();
--- dev/static/js/script.js
+++ dev/static/js/script.js
@@ -1,24 +1,84 @@
 module.exports = (function(){
 	// Variables
-	var var0,
-		var1,
-		var2;
+	var url = "https://f38a2491c6.execute-api.us-west-2.amazonaws.com/Sharp/form-submission",
+		form = $("#call-to-action-section form"),
+		client;
 
 	// Modules
 	var nav 		= require('./nav'),
 	 	utils	 	= require('./utils'),
-		jQPlugins 	= require('./jQPlugins');
+		jQPlugins 	= require('./jQPlugins'),
+		clientjs	= require('clientjs');
 
 	// Initiate
 	function init(){
-	
+		client = new ClientJS();
+	}
+
+	function post(){
+		var formData = form.serializeArray();
+		console.log("formData: \n" + JSON.stringify(formData, null, 4));
+		var metadata = getMetadata();
+		var data = {};
+		for (var i=0; i<formData.length;i++)
+			data[formData[i].name] = formData[i].value;
+		
+		data.fingerprint	= client.getFingerprint();
+		data.metadata 		= metadata;
+		if(requestbodyIsComplete(data))
+			sendRequest();
+		function sendRequest(){
+			var reqData = JSON.stringify(data, null, 4);
+			console.log(reqData);	
+			$.ajax({
+				type: "POST",
+				url: url,
+				data: reqData,
+				success: function(data, status, xhr){
+					alert("Thank you! We will be in touch soon.");
+					form.find("input[type=text], textarea").val("");
+					console.log(JSON.stringify(data));
+					console.log(JSON.stringify(status));
+				},
+				dataType: "json",
+				contentType : "application/json"
+			});
+		}
+	}
+	function getMetadata(){
+		var metadata = {};
+		metadata.name			= "metadata";
+		metadata.resolution		= client.getCurrentResolution();
+		metadata.os				= client.getOS();
+		metadata.browser		= client.getBrowser();
+		metadata.isMobile		= client.isMobile();
+		metadata.timeZone		= client.getTimeZone();
+		metadata.language		= client.getLanguage();
+		return metadata;
+	}
+	function requestbodyIsComplete(data){
+		if(data.name != "")
+			if(validateEmail(data.email))
+				return true;
+			else{
+				alert("Fill in email correctly, please");
+				return false;
+			}
+		alert("Fill in your name, please");
+		return false;
+		
+		function validateEmail(email){
+			return /\w+@\w+(\.\w+)+/.test(email);
+		}
 	}
 	// Return object	
 	var publicAPI = {
+		post:post,
 		init:init,
 		nav:nav,
 		utils:utils,
-		jQPlugins:jQPlugins
+		jQPlugins:jQPlugins,
+		client:client
 	};
 	return publicAPI;
 })();
